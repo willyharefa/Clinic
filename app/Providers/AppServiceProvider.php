d4483dd9a63ca9cddcc6b8f3224a17267decb17f
willyharefa <willyandrioharefa16@gmail.com>
2022-04-23 20:17:07 +0700

    Checkout back change htaccess

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
+
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,6 +26,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        URL::forceScheme('https');
     }
 }
